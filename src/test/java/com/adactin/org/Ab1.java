8337f7b9224b56536e29e2f341acd9400b2b493c
pavithra24297 <rajalakshmi2497@gmail.com>
2020-06-22 15:23:16 +0530

    first commit

---
+++ src/test/java/com/adactin/org/Ab1.java
@@ -0,0 +1,21 @@
+package com.adactin.org;
+
+public class Ab1 {
+	public static void star() {
+
+		for (int i = 0; i < 10; i++) {
+
+			for (int j = 0; j <= i; j++) {
+
+				System.out.print("*");
+
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+		star();
+
+	}
+}
